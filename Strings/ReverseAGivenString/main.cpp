e2379f57c835845166b1af692c57aaa53649955b
trevorsmthembu-spec <trevorsmthembu@gmail.com>
2026-07-05 13:00:49 +0200

    Add Reverse A Given String challenge

--- Strings/ReverseAGivenString/main.cpp
+++ Strings/ReverseAGivenString/main.cpp
@@ -2,14 +2,29 @@
 #include <string>
 using namespace std;
 
+// Function to reverse a string
+void reverseString(string strP)
+{
+    // Loop to reverse the string
+    for (int i = strP.length() - 1; i >= 0; i--) // Iterating through the string in reverse order
+    {
+        cout << strP[i]; //Printing out each letter/character index-by-index
+    }
+}
+
 int main ()
 {
-    string sample = "Reverse a Given String.";
+    string text = "Reverse a Given String.",
+           name = "Trevor";
 
-    for (int i = sample.length() - 1; i >= 0; i--)
-    {
-        cout << sample[i];
-    }
+    cout << "Original String: " << text; //Displaying the original string
+    cout << "\nReversed string: ";
+    reverseString(text); //Displaying the reversed string
+
+    
+    cout << "Original String: " << name; //Displaying the original string
+    cout << "\nReversed string: ";
+    reverseString(name); //Displaying the reversed string
 
     return 0;
 }
