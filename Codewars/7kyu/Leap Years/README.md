fe16bd83aba28365d079ba4ae2badd2b80eb6dd7
trevorsmthembu-spec <trevorsmthembu@gmail.com>
2026-07-05 14:43:49 +0200

    Add Codewars 7 kyu: Leap Years

---
+++ Codewars/7kyu/Leap Years/README.md
@@ -0,0 +1,8 @@
+# Leap Years (7 kyu)
+
+Determine whether a given year is a leap year using the standard leap year rules.
+
+## Concepts
+- Conditional statements
+- Modulo operator (`%`)
+- Boolean return values
---
+++ Codewars/7kyu/Leap Years/solution.cpp
@@ -0,0 +1,15 @@
+bool IsLeapYear(int year) 
+{
+  if (year % 4 == 0 && !(year % 100 == 0))
+    {
+    return true;
+  }
+  else if (year % 400 == 0)
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
